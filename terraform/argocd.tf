409efa446dfa0160ec6757338f60a171383204b1
Fedir Zadniprovskyi <github.g1k56@simplelogin.com>
2024-11-29 19:58:29 -0800

    init

---
+++ terraform/argocd.tf
@@ -0,0 +1,11 @@
+resource "helm_release" "argocd" {
+  name = "argocd"
+
+  repository       = "https://argoproj.github.io/argo-helm"
+  chart            = "argo-cd"
+  namespace        = "argocd"
+  create_namespace = true
+  version          = "7.7.6"
+
+  values = [file("values/argocd.yaml")]
+}
